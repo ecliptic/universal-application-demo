da681884c38ef7b1a87912d3494b147a4bea89ff
bkonkle <brandon@konkle.us>
2017-03-23 13:05:33 -0600

    Add missing files

---
+++ packages/universal-app-mobile/src/Components/Common/Container.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native'
+
+export default styled.View`
+  flex: 1;
+  align-items: center;
+  padding: 24 12;
+  background: black;
+  height: 100%;
+`
---
+++ packages/universal-app-mobile/src/Components/Common/Header.js
@@ -0,0 +1,9 @@
+import {Constants} from 'universal-app-core'
+import styled from 'styled-components/native'
+
+const {glowGreen} = Constants.COLORS
+
+export default styled.Text`
+  color: ${glowGreen};
+  font-size: 45;
+`
